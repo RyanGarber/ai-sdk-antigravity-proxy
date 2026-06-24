06f86e73fd60f000ab9017b652914b645cc690e7
Ryan <ryanmichaelgarber@gmail.com>
2026-06-24 00:04:47 -0400

    chore: add cjs definition, fix rootDir

--- tsup.config.js
+++ tsup.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
-  tsconfig: "tsconfig.tsup.json",
   noExternal: ["antigravity-proxy"],
   dts: {
     compilerOptions: {
